66a6b3f63b45250b07097becfe1bafaf15930487
Sumukh Prasad <sumukhprasad@icloud.com>
2026-08-26 05:22:47 +0530

    Added ability to write header.

--- assemble.rb
+++ assemble.rb
@@ -5,9 +5,18 @@ class ColourAssembler
 	attr_reader :colours
 	attr_reader :version
 	
-	def initialize
+	def initialize(variable_prefix: "$", colour_prefix: "color-", colour_postfix: "", comment_prefix: "/* ", comment_newline_prefix: " * ", comment_postfix: " */", output_file: "_vars.sass")
 		@colours = {}
 		@version = "0.0.1"
+
+		@variable_prefix = variable_prefix
+		@colour_prefix = colour_prefix
+		@colour_postfix = colour_postfix
+		@comment_prefix = comment_prefix
+		@comment_newline_prefix = comment_newline_prefix
+		@comment_postfix = comment_postfix
+		@output_file = output_file
+		
 	end
 	
 	def get_colour_group_info_from_file(f_relative_path)
@@ -32,10 +41,41 @@ class ColourAssembler
 		
 		puts "Added group."
 	end
+	
+	def generate_stylesheet
+		file = File.join(File.dirname(__FILE__), @output_file)
+		File.open(file, 'w') { |f| 
+			write_header(f)
+		}
+	end
+	
+	
+	private
+	def write_header(fo)
+		header = <<~EOS
+		#{@comment_prefix}
+		#{@comment_newline_prefix} Colourtaculous!
+		#{@comment_newline_prefix} (version #{@version})
+		#{@comment_newline_prefix}
+		#{@comment_newline_prefix} Colour palatte generated from #{auto_pluralise(@colours.keys.length, "group")}.
+		#{@comment_postfix}
+		EOS
+		fo.puts header
+	end
+	
+	def auto_pluralise(n, singular, plural=nil)
+		if n == 1
+			"1 #{singular}"
+		elsif plural
+			"#{n} #{plural}"
+		else
+			"#{n} #{singular}s"
+		end
+	end
 end
 
 
 assembler = ColourAssembler.new
 
 assembler.get_colour_group_info_from_file("claret")
-puts assembler.colours
+assembler.generate_stylesheet
